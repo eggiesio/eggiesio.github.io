c604ac08e3a6f818b84725139b93bc094fa7db39
eggiesio <eggies.co@gmail.com>
2025-10-27 21:51:22 +0500

    New home page

---
+++ assets/apps.ts
@@ -0,0 +1,387 @@
+export const APPS = 
+[
+  {
+    "img": "io.ionic.emojiquiz.png",
+    "price": "free",
+    "name": "Emoji Quiz - Guess the Emojis",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=io.ionic.emojiquiz&hl=en&gl=us",
+    "shortDesc": "Solve 1400+ tricky emoji puzzles &amp; become emoji guess master."
+  },
+  {
+    "img": "com.eggies.makeme10.png",
+    "price": "free",
+    "name": "Make Me 10!",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.makeme10&hl=en&gl=us",
+    "shortDesc": "Merge numbers to make 10. Can you do it?"
+  },
+  {
+    "img": "com.eggies.flashmemory.png",
+    "price": "free",
+    "name": "Flash Pairs - Matching Game",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.flashmemory&hl=en&gl=us",
+    "shortDesc": "Train your brain's focus and concentration with Flash Pairs A Matching Game!"
+  },
+  {
+    "img": "com.eggies.riddlemejustriddles.png",
+    "price": "free",
+    "name": "Riddle Me - A Game of Riddles",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.riddlemejustriddles&hl=en&gl=us",
+    "shortDesc": "Are you a Riddle Solver? Solve 5000+ tricky riddles &amp; become a Riddle Master!"
+  },
+  {
+    "img": "com.eggies.colorspin.png",
+    "price": "paid",
+    "name": "Color Spin",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.colorspin&hl=en&gl=us",
+    "shortDesc": "A meditative &amp; relaxing color puzzle about rotating discs &amp; matching colors."
+  },
+  {
+    "img": "com.eggies.newbrainyfour.png",
+    "price": "free",
+    "name": "Brainy four: Four letter words",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.newbrainyfour&hl=en&gl=us",
+    "shortDesc": "Challenge your brain by arranging four letters into sensible words."
+  },
+  {
+    "img": "com.eggies.emojichallenge.png",
+    "price": "paid",
+    "name": "Emoji Guess Challenge",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.emojichallenge&hl=en&gl=us",
+    "shortDesc": "Guess the emojis in 1400+ challenging emoji puzzles and become emoji master!"
+  },
+  {
+    "img": "com.eggies.spellingmaster.png",
+    "price": "paid",
+    "name": "Spelling Master - Quiz Games",
+    "category": "Educational",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.spellingmaster&hl=en&gl=us",
+    "shortDesc": "Play to Learn English Spelling &amp; Grammar!"
+  },
+  {
+    "img": "com.eggies.triviachristmasgame.png",
+    "price": "free",
+    "name": "Christmas Trivia - Quiz Game",
+    "category": "Trivia",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.triviachristmasgame&hl=en&gl=us",
+    "shortDesc": "Unwrap the Festive Fun : Become the Christmas Quiz Champion!"
+  },
+  {
+    "img": "com.eggies.truthordare.png",
+    "price": "free",
+    "name": "Truth or Dare: Party Game",
+    "category": "Casual",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.truthordare&hl=en&gl=us",
+    "shortDesc": "Truth or Dare party game with 5000+ exciting challenges for kids, teens &amp; adults"
+  },
+  {
+    "img": "com.eggies.onetohundredlight.png",
+    "price": "free",
+    "name": "Find Numbers - Brain Challenge",
+    "category": "Educational",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.onetohundredlight&hl=en&gl=us",
+    "shortDesc": "Find the Number: The Ultimate Number Finding and Speed Reading Challenge!"
+  },
+  {
+    "img": "com.eggies.minicrossword.png",
+    "price": "paid",
+    "name": "Mini Crossword - Word Fun!",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.minicrossword&hl=en&gl=us",
+    "shortDesc": "Unravel word puzzles in Mini Crossword. Challenge your brain!"
+  },
+  {
+    "img": "com.eggies.neverhaveiever.png",
+    "price": "free",
+    "name": "Never Ever - Party Game",
+    "category": "Casual",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.neverhaveiever&hl=en&gl=us",
+    "shortDesc": "Have you ever played this game... Never or I have?"
+  },
+  {
+    "img": "com.eggies.onetohundred.png",
+    "price": "free",
+    "name": "Find numbers: 1 to 100",
+    "category": "Educational",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.onetohundred&hl=en&gl=us",
+    "shortDesc": "Find numbers from one to hundred in 8  exciting game modes."
+  },
+  {
+    "img": "com.eggies.speedmathpaid.png",
+    "price": "paid",
+    "name": "Speed Math - Mini Math Games",
+    "category": "Educational",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.speedmathpaid&hl=en&gl=us",
+    "shortDesc": "Choosing true or false within a second can make you freaking mad!"
+  },
+  {
+    "img": "com.eggies.parity.png",
+    "price": "free",
+    "name": "Parity - Numbers game",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.parity&hl=en&gl=us",
+    "shortDesc": "Train your brain by playing parity: An addictive &amp; unique numbers puzzle game."
+  },
+  {
+    "img": "com.eggies.sciencemaster.png",
+    "price": "paid",
+    "name": "Science Master - Quiz Games",
+    "category": "Trivia",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.sciencemaster&hl=en&gl=us",
+    "shortDesc": "Explore 9500+ Science Questions for Enhanced Knowledge."
+  },
+  {
+    "img": "com.eggies.emojimatch.png",
+    "price": "free",
+    "name": "Emoji Match - A Sliding Puzzle",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.emojimatch&hl=en&gl=us",
+    "shortDesc": "Be a slider master and put all the emojis in their homes."
+  },
+  {
+    "img": "com.eggies.simonsaysrememberandrepeat.png",
+    "price": "paid",
+    "name": "Bulbs - A game of lights",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.simonsaysrememberandrepeat&hl=en&gl=us",
+    "shortDesc": "Just watch, remember and repeat the sequence of blinking bulbs."
+  },
+  {
+    "img": "com.eggies.crazycalculator.png",
+    "price": "paid",
+    "name": "Crazy Calculator Game",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.crazycalculator&hl=en&gl=us",
+    "shortDesc": "You sure have used calculators before, but this one is crazy, this one is a game"
+  },
+  {
+    "img": "com.eggies.wordmania.png",
+    "price": "free",
+    "name": "Word Mania - Brainy Word Games",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.wordmania&hl=en&gl=us",
+    "shortDesc": "7 challenging mini word games in a single game pack. Perfect for brain training!"
+  },
+  {
+    "img": "com.eggies.wordslice.png",
+    "price": "paid",
+    "name": "Word Slice: 1 Hint 5 Words!",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.wordslice&hl=en&gl=us",
+    "shortDesc": "Slice, Solve, and Conquer: 1 Hint, 5 Words! Unleash Your Word Prowess Offline."
+  },
+  {
+    "img": "com.eggies.logoguesschallenge.png",
+    "price": "paid",
+    "name": "Logo Guess Challenge",
+    "category": "Trivia",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.logoguesschallenge&hl=en&gl=us",
+    "shortDesc": "Guess over 2000 logos of brands from all over the world and have fun!"
+  },
+  {
+    "img": "com.eggies.sportsmaster.png",
+    "price": "paid",
+    "name": "Sports Master - Quiz Games",
+    "category": "Trivia",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.sportsmaster&hl=en&gl=us",
+    "shortDesc": "Test your sports knowledge with Sports Master, the ultimate quiz game."
+  },
+  {
+    "img": "com.eggies.spacerush.png",
+    "price": "paid",
+    "name": "Space Rush",
+    "category": "Casual",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.spacerush&hl=en&gl=us",
+    "shortDesc": "A hyper casual game that will challenge your reflexes &amp; reaction skills."
+  },
+  {
+    "img": "com.eggies.techquizmaster.png",
+    "price": "free",
+    "name": "Tech Quiz Master - Quiz Games",
+    "category": "Educational",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.techquizmaster&hl=en&gl=us",
+    "shortDesc": "Test your tech knowledge about latest technologies and become a real tech geek."
+  },
+  {
+    "img": "com.eggies.get10.png",
+    "price": "free",
+    "name": "Just Get 10! - Sketchy",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.get10&hl=en&gl=us",
+    "shortDesc": "Find identical numbers and combine them to grow up. See if you can get 10!"
+  },
+  {
+    "img": "com.eggies.triviamaster.png",
+    "price": "paid",
+    "name": "Trivia Master - Quiz Games",
+    "category": "Trivia",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.triviamaster&hl=en&gl=us",
+    "shortDesc": "Improve your general knowledge in this multiple choice trivia game."
+  },
+  {
+    "img": "com.eggies.taprush.png",
+    "price": "paid",
+    "name": "Tap Rush - Reflex Game",
+    "category": "Arcade",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.taprush&hl=en&gl=us",
+    "shortDesc": "Tap with Precision: Master the Reflex Challenge in Style!"
+  },
+  {
+    "img": "com.eggies.wouldyourather.png",
+    "price": "free",
+    "name": "Would You Rather: Hard choices",
+    "category": "Casual",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.wouldyourather&hl=en&gl=us",
+    "shortDesc": "Would you rather choose this or that?"
+  },
+  {
+    "img": "com.eggies.mathriddles.png",
+    "price": "paid",
+    "name": "Math Riddles: Math Me",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.mathriddles&hl=en&gl=us",
+    "shortDesc": "Train your brain &amp; boost IQ with 500+ math riddles, logic puzzles &amp; fun facts!"
+  },
+  {
+    "img": "com.eggies.mastermind.png",
+    "price": "free",
+    "name": "Mastermind Remastered",
+    "category": "Board",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.mastermind&hl=en&gl=us",
+    "shortDesc": "Keep calm and play Mastermind Remastered!"
+  },
+  {
+    "img": "com.eggies.thisorthat.png",
+    "price": "free",
+    "name": "This or That Game",
+    "category": "Casual",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.thisorthat&hl=en&gl=us",
+    "shortDesc": "What's your choice...This or That?"
+  },
+  {
+    "img": "com.eggies.blockblast.png",
+    "price": "paid",
+    "name": "Block Blast - A Retro Game",
+    "category": "Arcade",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.blockblast&hl=en&gl=us",
+    "shortDesc": "Let's check your reflexes, speed and reaction against these fast pacing blocks!"
+  },
+  {
+    "img": "com.eggies.animalsmemorygame.png",
+    "price": "free",
+    "name": "Memorize - Pairs matching game",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.animalsmemorygame&hl=en&gl=us",
+    "shortDesc": "Match images in this fun, challenging game that boosts focus and concentration!"
+  },
+  {
+    "img": "com.eggies.wordquest.png",
+    "price": "paid",
+    "name": "Word Quest - Decode the Clues!",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.wordquest&hl=en&gl=us",
+    "shortDesc": "Word Quest: Connect the Clues, Decode the Fun!"
+  },
+  {
+    "img": "com.eggies.wordleinfinity.png",
+    "price": "free",
+    "name": "Woriddle: Word Guess Challenge",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.wordleinfinity&hl=en&gl=us",
+    "shortDesc": "Play unlimited daily word puzzles to train your brain and boost your vocabulary!"
+  },
+  {
+    "img": "com.eggies.knobsters.png",
+    "price": "paid",
+    "name": "Knobsters",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.knobsters&hl=en&gl=us",
+    "shortDesc": "Rotate and make all knobs the same color."
+  },
+  {
+    "img": "com.eggies.wordwizard.png",
+    "price": "free",
+    "name": "Word wizard - A word challenge",
+    "category": "Word",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.wordwizard&hl=en&gl=us",
+    "shortDesc": "Try at least once and get surprised by this magical word game."
+  },
+  {
+    "img": "com.eggies.snakesladders.png",
+    "price": "free",
+    "name": "Snakes and ladders king - Sket",
+    "category": "Board",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.snakesladders&hl=en&gl=us",
+    "shortDesc": "Enjoy playing classic snakes and ladders board game."
+  },
+  {
+    "img": "com.eggies.matchpairs.png",
+    "price": "free",
+    "name": "Pair Up - Emoji Match",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.matchpairs&hl=en&gl=us",
+    "shortDesc": "Match emoji pairs and sharpen your focus in this classic matching game!"
+  },
+  {
+    "img": "com.eggies.mathgame.png",
+    "price": "free",
+    "name": "Math Game 2019",
+    "category": "Educational",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.mathgame&hl=en&gl=us",
+    "shortDesc": "Master your math skills by playing this crazy math game 2019 edition."
+  },
+  {
+    "img": "com.eggies.puzzle15.png",
+    "price": "free",
+    "name": "Puzzle 15 - A sliding puzzle g",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.puzzle15&hl=en&gl=us",
+    "shortDesc": "Enjoy playing classic sliding tiles game: puzzle 15 from 8 tiles up to 48 tiles."
+  },
+  {
+    "img": "com.eggies.colorblind.png",
+    "price": "free",
+    "name": "Color blind",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.colorblind&hl=en&gl=us",
+    "shortDesc": "Tap the color you see not the color you read"
+  },
+  {
+    "img": "com.eggies.emojitictactoe.png",
+    "price": "free",
+    "name": "Emoji Tic Tac Toe",
+    "category": "Strategy",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.emojitictactoe&hl=en&gl=us",
+    "shortDesc": "Stop wasting paper and start playing classic tic tac toe emoji version."
+  },
+  {
+    "img": "com.eggies.lettersfire.png",
+    "price": "free",
+    "name": "Letters Fire - True Or False",
+    "category": "Puzzle",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.lettersfire&hl=en&gl=us",
+    "shortDesc": "Choosing true or false in letters fire can drive you crazy!"
+  },
+  {
+    "img": "com.eggies.cssgenerator.png",
+    "price": "free",
+    "name": "CSS Button Generator",
+    "category": "Education",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.cssgenerator&hl=en&gl=us",
+    "shortDesc": "Design beautiful CSS3 buttons on your android and use them in any web project."
+  },
+  {
+    "img": "com.eggies.illuminatiornot.png",
+    "price": "free",
+    "name": "Illuminati or Not - Prank",
+    "category": "Entertainment",
+    "url": "https://play.google.com/store/apps/details?id=com.eggies.illuminatiornot&hl=en&gl=us",
+    "shortDesc": "Are you illuminati? Check now!"
+  }
+]
